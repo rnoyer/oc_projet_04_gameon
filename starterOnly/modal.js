fa13261726d6c65555f27a0e4ec072d58000d8f2
romain noyer <romain.noyer@gmail.com>
2024-02-21 12:16:36 +0100

    ADD success modal and FIX check form validator

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,6 +22,8 @@ const date = document.querySelector("#birthdate"); // (rno) Store form ...
 const quantity = document.querySelector("#quantity"); // (rno) Store form ...
 const radioButtons = document.querySelectorAll(".checkbox-input[type='radio']"); // (rno) Store form ...
 const checkboxTerms = document.querySelector("#checkbox1"); // (rno) Store form ...
+const allInputs = document.querySelectorAll("input"); // (rno) Store form ...
+const modal = document.querySelector("#modal-body");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -40,29 +42,31 @@ function closeModal() {
 // ---------------
 
 // Check names format
-function isNameValid(name) {
+function checkName(name) {
   const regex = new RegExp("^[a-zA-Z]+(?:[' -][a-zA-Z]+)*$");
   return name.length >= 2 && regex.test(name);
 }
 // Check email format
-function isEmailValid(email) {
-  const emailRegEx = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
+function checkEmail(email) {
+  const emailRegEx = new RegExp(
+    "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$"
+  );
   return emailRegEx.test(email);
 }
 // Check date format
-function isDateValid(date) {
+function checkDate(date) {
   const dateRegEx = new RegExp("^\\d{4}-\\d{2}-\\d{2}$");
   return dateRegEx.test(date);
 }
 // Check Quantity
-function isQuantityValid(quantity) {
-  if (!!quantity && quantity >= 0 && quantity <= 99){
+function checkQuantity(quantity) {
+  if (!!quantity && quantity >= 0 && quantity <= 99) {
     return true;
   }
   return false;
 }
-// Check radio buttons for location
-function isLocationChecked(radioButtons){
+// Check that one radio button is checked
+function checkLocation(radioButtons) {
   for (let i = 0; i < radioButtons.length; i++) {
     const radioButton = radioButtons[i];
     if (radioButton.checked) {
@@ -72,178 +76,105 @@ function isLocationChecked(radioButtons){
   return false;
 }
 // Check Terms checkbox
-function isAcceptedTerms(checkbox){
-  if(checkbox.checked){
+function checkTerms(checkbox) {
+  if (checkbox.checked) {
     return true;
   }
   return false;
 }
 
 // ---------------
-// FORM VALIDATION FONCTIONS
+// FIELD VALIDATION FONCTION
 // ---------------
 
-function validateFirstNameField() {
-  const isFirstNameValid = isNameValid(firstName.value);
-  const firstNameFormData = firstName.parentNode;
-  if (!isFirstNameValid) {
-    firstNameFormData.setAttribute(
-      "data-error",
-      "Veuillez entrer 2 caractères ou plus pour le champ du Prénom."
-    );
-    firstNameFormData.setAttribute("data-error-visible", "true");
+function checkAndValidateField(validationData) {
+  const { isValidField, inputElement, alertMessage, type } = validationData;
+  // Certaines fonctions de validation ont besoin du value de l'élément, d'autres de l'élément lui-même
+  const isValid = isValidField(
+    type === "radios" || type === "checkbox" ? inputElement : inputElement.value
+  );
+
+  // Pour les radios on doit récupérer un élément du tableau pour avoir accès à parentNode
+  const formDataElement =
+    type === "radios" ? inputElement[0].parentNode : inputElement.parentNode;
+
+  if (!isValid) {
+    formDataElement.setAttribute("data-error", alertMessage);
+    formDataElement.setAttribute("data-error-visible", "true");
   } else {
-    firstNameFormData.removeAttribute("data-error");
-    firstNameFormData.removeAttribute("data-error-visible");
+    formDataElement.removeAttribute("data-error");
+    formDataElement.removeAttribute("data-error-visible");
   }
-}
-
-function validateLastNameField() {
-  const isLastNameValid = isNameValid(lastName.value);
-  const lastNameFormData = lastName.parentNode;
-  if (!isLastNameValid) {
-    lastNameFormData.setAttribute(
-      "data-error",
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom."
-    );
-    lastNameFormData.setAttribute("data-error-visible", "true");
-  } else {
-    lastNameFormData.removeAttribute("date-error");
-    lastNameFormData.removeAttribute("data-error-visible");
-  }
-}
-
-function validateEmailField() {
-  const isEmailAddressValid = isEmailValid(email.value);
-  const emailFormData = email.parentNode;
-  if (!isEmailAddressValid) {
-    emailFormData.setAttribute(
-      "data-error",
-      "Veuillez entrer une adresse email valide."
-    );
-    emailFormData.setAttribute("data-error-visible", "true");
-  } else {
-    emailFormData.removeAttribute("data-error");
-    emailFormData.removeAttribute("data-error-visible");
-  }
-}
-
-function validateBirthdateField() {
-  const isBirthdateValid = isDateValid(date.value);
-  const dateFormData = date.parentNode;
-  if(!isBirthdateValid){
-    dateFormData.setAttribute("data-error","Veuillez entrer un format de date valide.");
-    dateFormData.setAttribute("data-error-visible", "true");
-  }else{
-    dateFormData.removeAttribute("data-error");
-    dateFormData.removeAttribute("data-error-visible");
-  }
-}
-
-function validateQuantityField() {
-  const quantityValid = isQuantityValid(quantity.value)
-  const quantityFormData = quantity.parentNode;
-  if (!quantityValid) {
-    quantityFormData.setAttribute("data-error", "Veuillez entrer un nombre entre 0 et 99");
-    quantityFormData.setAttribute("data-error-visible", "true");
-  } else {
-    quantityFormData.removeAttribute("data-error");
-    quantityFormData.removeAttribute("data-error-visible");
-  }
- }
-
-function validateLocationRadioButton() {
-  const isLocationOk = isLocationChecked(radioButtons)
-  const locationFormData = radioButtons[0].parentNode;
-  if (!isLocationOk) {
-    locationFormData.setAttribute("data-error", "Choisissez un lieu");
-    locationFormData.setAttribute("data-error-visible", "true");
-  } else {
-    locationFormData.removeAttribute("data-error");
-    locationFormData.removeAttribute("data-error-visible");
-  }
-}
-
-function validateTermsCheckbox() {
-  const isTermChecked = isAcceptedTerms(checkboxTerms);
-  const termsFormData = checkboxTerms.parentNode;
-  if (!isTermChecked) {
-    termsFormData.setAttribute("data-error", "Vous devez accepter les conditions");
-    termsFormData.setAttribute("data-error-visible", "true");
-  } else {
-    termsFormData.removeAttribute("data-error");
-    termsFormData.removeAttribute("data-error-visible");
-  }
-}
-
-
-function checkAndValidateField(rule, ruleParam, DOMElement, alertMessage){
-  console.log("ruleParam: " + ruleParam)
-  const checkRule = rule(ruleParam);
-  const elementFormData = DOMElement.parentNode;
-  if (!checkRule) {
-    elementFormData.setAttribute("data-error", alertMessage);
-    elementFormData.setAttribute("data-error-visible", "true");
-  } else {
-    elementFormData.removeAttribute("data-error");
-    elementFormData.removeAttribute("data-error-visible");
-  }
+  return isValid
 }
 
 const fieldParams = {
-  firstName: [isNameValid, firstName.value, firstName, "message test"],
+  first: {
+    isValidField: checkName,
+    inputElement: firstName,
+    alertMessage:
+      "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.",
+  },
+  last: {
+    isValidField: checkName,
+    inputElement: lastName,
+    alertMessage: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+  },
+  email: {
+    isValidField: checkEmail,
+    inputElement: email,
+    alertMessage: "Veuillez entrer une adresse email valide.",
+  },
+  birthdate: {
+    isValidField: checkDate,
+    inputElement: date,
+    alertMessage: "Veuillez entrer un format de date valide.",
+  },
+  quantity: {
+    isValidField: checkQuantity,
+    inputElement: quantity,
+    alertMessage: "Veuillez entrer un nombre entre 0 et 99",
+  },
+  location: {
+    isValidField: checkLocation,
+    inputElement: radioButtons,
+    type: "radios",
+    alertMessage: "Choisissez un lieu",
+  },
+  checkbox1: {
+    isValidField: checkTerms,
+    inputElement: checkboxTerms,
+    type: "checkbox",
+    alertMessage: "Vous devez accepter les conditions",
+  },
 };
 
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // validateFirstNameField();
-  checkAndValidateField(...fieldParams.firstName);
+  let firstIsValid = checkAndValidateField(fieldParams.first);
+  let lastIsValid = checkAndValidateField(fieldParams.last);
+  let emailIsValid = checkAndValidateField(fieldParams.email);
+  let birthdateIsValid = checkAndValidateField(fieldParams.birthdate);
+  let quantityIsValid = checkAndValidateField(fieldParams.quantity);
+  let locationIsValid = checkAndValidateField(fieldParams.location);
+  let checkbox1IsValid = checkAndValidateField(fieldParams.checkbox1);
 
-  validateLastNameField();
-  validateEmailField();
-  validateBirthdateField();
-  validateQuantityField();
-  validateLocationRadioButton();
-  validateTermsCheckbox();
+  if (firstIsValid && lastIsValid && emailIsValid && birthdateIsValid && quantityIsValid && locationIsValid && checkbox1IsValid){
+    successModal()
+  }
 });
 
-firstName.addEventListener("blur", () => {
-  validateFirstNameField();
+allInputs.forEach((input) => {
+  input.addEventListener("change", () => {
+    // Selon l'ID de l'input, on récupère les paramètres de validation correspondant
+    checkAndValidateField(
+      fieldParams[input.id.startsWith("location") ? "location" : input.id]
+    );
+  });
 });
 
-lastName.addEventListener("blur", () => {
-  validateLastNameField();
-});
-
-email.addEventListener("blur", () => {
-  validateEmailField();
-});
-
-birthdate.addEventListener("blur", () => {
-  validateBirthdateField();
-});
-
-quantity.addEventListener("blur", () => {
-  validateQuantityField();
-});
-
-// Object.entries(formFields).forEach(entry => {
-//   const [field, validateField] = entry;
-//   field.addEventListener("blur", () => {
-//     validateField();
-//   });
-// });
-
-
-
-// const formFields = {
-//   //Each field take an array of parameters : [<validate_function>,<string:error_message>,<bool:hasBlurListenerr> ]
-//   firstName: [isNameValid, "firstName.value", "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.", true],
-//   lastName: [isNameValid, "lastName.value", "Veuillez entrer 2 caractères ou plus pour le champ du nom.", true],
-//   email: [isEmailValid, "email.value", "Veuillez entrer une adresse email valide.", true],
-//   birthdate: [isDateValid, "date.value", "Veuillez entrer un format de date valide.", true],
-//   quantity: [isQuantityValid, "quantity.value", "Veuillez entrer un nombre entre 0 et 99", true],
-//   radioButtons: [isLocationChecked, "radioButtons", "Choisissez un lieu", false],
-//   checkboxTerms: [isAcceptedTerms, "checkboxTerms", "Vous devez accepter les conditions", false],
-// };
+// Fonction qui ajoute le contenu de succes
+function successModal(){
+  modal.innerHTML = '<p>Merci ! Votre réservation a été reçue.</p>';
+}
